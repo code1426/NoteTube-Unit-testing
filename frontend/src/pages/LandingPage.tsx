dcd6f098252fbaf1d9fc382f230fa7e21cb8d212
Jose <joseearronn@gmail.com>
2024-11-11 18:02:18 +0800

    Add sample font

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -5,23 +5,23 @@ const LandingPage = () => {
         <div className="self-stretch px-9 py-4 justify-between items-center inline-flex">
           <div className="w-[75px] h-[75px] bg-[#03c04a] rounded-full" />
           <div className="self-stretch px-4 justify-end items-center gap-9 flex">
-            <button className="text-[#03c04a] text-[28px] font-medium font-['Inter']">
+            <button className="text-[#03c04a] text-[28px] font-primaryRegular">
               About
             </button>
-            <button className="text-[#03c04a] text-[28px] font-medium font-['Inter']">
+            <button className="text-[#03c04a] text-[28px] font-primaryRegular">
               Features
             </button>
           </div>
           <div className="self-stretch px-4 justify-end items-center gap-9 flex">
             <div className="w-[204px] h-20 relative">
               <button className="w-[204px] h-20 left-0 top-0 absolute bg-[#03c04a] rounded-[50px]" />
-              <div className="left-[45px] top-[23px] absolute text-neutral-50 text-[28px] font-medium font-['Inter']">
+              <div className="left-[45px] top-[23px] absolute text-neutral-50 text-[28px] font-primaryRegular">
                 Register
               </div>
             </div>
             <div className="w-[204px] h-20 relative">
               <button className="w-[204px] h-20 left-0 top-0 absolute bg-white rounded-[50px] border border-[#03c04a]" />
-              <div className="left-[68px] top-[23px] absolute text-[#03c04a] text-[28px] font-medium font-['Inter']">
+              <div className="left-[68px] top-[23px] absolute text-[#03c04a] text-[28px] font-primaryRegular">
                 Log In
               </div>
             </div>
@@ -30,17 +30,17 @@ const LandingPage = () => {
         <div className="self-stretch h-[4320px] px-6 flex-col justify-start items-center gap-[101px] inline-flex">
           <div className="w-[screen] h-[735.42px] py-24 flex-col justify-center items-center flex">
             <div className="h-[568px] flex-col justify-end items-center flex">
-              <div className="w-[936px] h-[329px] text-center text-[#03c04a] text-9xl font-extrabold font-['Lexend']">
+              <div className="w-[936px] h-[329px] text-center text-[#03c04a] text-9xl font-secondaryRegular">
                 Welcome To
                 <br />
                 NoteTube
               </div>
-              <div className="w-[978px] h-[139px] text-center text-[#03c04a] text-[40px] font-normal font-['Lexend']">
+              <div className="w-[978px] h-[139px] text-center text-[#03c04a] text-[40px] font-secondaryRegular">
                 Your Ai-Powered Study Companion
               </div>
               <div className="w-[400px] h-[100px] relative">
                 <button className="w-[400px] h-[100px] left-0 top-0 absolute bg-[#03c04a] rounded-[50px]" />
-                <div className="left-[100px] top-[28px] absolute text-[#f0fffa] text-4xl font-bold font-['Inter']">
+                <div className="left-[100px] top-[28px] absolute text-[#f0fffa] text-4xl font-secondaryRegular">
                   Get Started
                 </div>
               </div>
@@ -48,10 +48,10 @@ const LandingPage = () => {
           </div>
           <div className="self-stretch px-9 py-[50px] bg-[#03c04a] justify-center items-center gap-[124px] inline-flex">
             <div className="grow shrink basis-0 self-stretch flex-col justify-center items-center gap-[104px] inline-flex">
-              <div className="self-stretch text-center text-white text-8xl font-semibold font-['Lexend']">
+              <div className="self-stretch text-center text-white text-8xl font-secondaryRegular">
                 What is NoteTube all about?
               </div>
-              <div className="self-stretch text-justify text-black text-[32px] font-normal font-['Inter']">
+              <div className="self-stretch text-justify text-black text-[32px] font-primaryRegular">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
                 et aliquam lectus. Integer at tellus consequat, egestas elit ac,
                 eleifend erat. Praesent ullamcorper ornare fermentum. Fusce
@@ -64,10 +64,10 @@ const LandingPage = () => {
           </div>
           <div className="self-stretch px-9 py-[50px] justify-center items-center gap-[124px] inline-flex">
             <div className="grow shrink basis-0 self-stretch flex-col justify-center items-center gap-[104px] inline-flex">
-              <div className="self-stretch text-center text-[#03c04a] text-8xl font-semibold font-['Lexend']">
+              <div className="self-stretch text-center text-[#03c04a] text-8xl font-secondaryRegular">
                 Upload Notes
               </div>
-              <div className="self-stretch text-black text-[32px] font-normal font-['Inter']">
+              <div className="self-stretch text-black text-[32px] font-primaryRegular">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
                 et aliquam lectus. Integer at tellus consequat, egestas elit ac,
                 eleifend erat. Praesent ullamcorper ornare fermentum. Fusce
@@ -80,10 +80,10 @@ const LandingPage = () => {
           </div>
           <div className="self-stretch px-9 py-[50px] bg-[#03c04a] justify-center items-center gap-[124px] inline-flex">
             <div className="grow shrink basis-0 self-stretch flex-col justify-center items-center gap-[104px] inline-flex">
-              <div className="self-stretch text-center text-white text-8xl font-semibold font-['Lexend']">
+              <div className="self-stretch text-center text-white text-8xl font-secondaryRegular">
                 Get Related Videos
               </div>
-              <div className="self-stretch text-black text-[32px] font-normal font-['Inter']">
+              <div className="self-stretch text-black text-[32px] font-primaryRegular">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
                 Aliquam et aliquam lectus. Integer at tellus consequat, <br />
                 egestas elit ac, eleifend erat. Praesent ullamcorper ornare
@@ -96,10 +96,10 @@ const LandingPage = () => {
           </div>
           <div className="self-stretch px-9 py-[50px] justify-center items-center gap-[124px] inline-flex">
             <div className="grow shrink basis-0 self-stretch flex-col justify-center items-center gap-[104px] inline-flex">
-              <div className="self-stretch text-center text-[#03c04a] text-8xl font-semibold font-['Lexend']">
+              <div className="self-stretch text-center text-[#03c04a] text-8xl font-secondaryRegular">
                 Use Generated Flashcards
               </div>
-              <div className="self-stretch text-black text-[32px] font-normal font-['Inter']">
+              <div className="self-stretch text-black text-[32px] font-primaryRegular">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
                 et aliquam lectus. Integer at tellus consequat, egestas elit ac,
                 eleifend erat. Praesent ullamcorper ornare fermentum. Fusce
