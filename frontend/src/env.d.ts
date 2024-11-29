de99aa8e381dcac65b944198e7b4386c67bf9a68
FrancyAmada <francyamada1983@gmail.com>
2024-11-30 03:56:16 +0800

    fixed conflict

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -2,8 +2,6 @@
 
 interface ImportMetaEnv {
   readonly VITE_BASE_API_URL: string;
-  readonly VITE_YOUTUBE_SEARCH_API_URL: string;
-  readonly VITE_YOUTUBE_API_KEY: string;
 }
 
 interface ImportMeta {
