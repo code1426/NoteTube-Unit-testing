3f30b89bf53c7d68f3dd392758acb645e3298f1f
Jose <joseearronn@gmail.com>
2024-11-30 02:53:16 +0800

    small fix

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_BASE_API_URL: string;
   readonly VITE_GEMINI_API_KEY: string;
+  readonly VITE_YOUTUBE_SEARCH_API_URL: string;
+  readonly VITE_YOUTUBE_API_KEY: string;
 }
 
 interface ImportMeta {
