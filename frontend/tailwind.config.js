17893c27a0d242762301a464e2300def4565fa20
Jose <joseearronn@gmail.com>
2024-12-02 20:44:01 +0800

    Setup shadcn

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   options: {
     safelist: ["visible", "animate-bounceIn"],
@@ -9,38 +10,45 @@ export default {
       cursor: {
         chillguy: "url(assets/images/chillguy.png), auto",
       },
-      // height: {
-      //   24: "6rem",
-      //   20: "5rem",
-      //   16: "4rem",
-      //   12: "3rem",
-      //   8: "2rem",
-      // },
       screens: {
-        xs: "480px", // Extra small screens, mobile devices (portrait)
-        sm: "640px", // Small screens, like larger smartphones
-        "sm-md": "700px", // Small-to-medium range (new custom breakpoint)
-        md: "768px", // Medium screens, tablets in portrait mode
-        "md-lg": "900px", // Medium-to-large range (new custom breakpoint)
-        lg: "1024px", // Large screens, tablets in landscape mode, small desktops
-        "lg-xl": "1100px", // Large-to-extra large range (new custom breakpoint)
-        xl: "1280px", // Extra large screens, typical desktops and laptops
-        "2xl": "1536px", // Very large screens, high-res displays, larger desktops
-        "3xl": "1920px", // Custom 3xl breakpoint
-        "4xl": "2560px", // Custom 4xl breakpoint
-        "4k": "3840px", // 4K screens, ultra-high-definition screens
-        xxl: "5000px", // Extreme custom breakpoint (example)
+        xs: "480px",
+        sm: "640px",
+        "sm-md": "700px",
+        md: "768px",
+        "md-lg": "900px",
+        lg: "1024px",
+        "lg-xl": "1100px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1920px",
+        "4xl": "2560px",
+        "4k": "3840px",
+        xxl: "5000px",
       },
       keyframes: {
         flip: {
-          "0%": { transform: "rotateY(60deg)" },
-          "75%": { transform: "rotateY(120deg)" },
-          "100%": { transform: "rotateY(180deg)" },
+          "0%": {
+            transform: "rotateY(60deg)",
+          },
+          "75%": {
+            transform: "rotateY(120deg)",
+          },
+          "100%": {
+            transform: "rotateY(180deg)",
+          },
         },
         bounceIn: {
-          "0%": { transform: "translateY(50px)", opacity: "0" },
-          "50%": { transform: "translateY(-10px)" },
-          "100%": { transform: "translateY(0)", opacity: "1" },
+          "0%": {
+            transform: "translateY(50px)",
+            opacity: "0",
+          },
+          "50%": {
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
         },
       },
       animation: {
@@ -54,6 +62,46 @@ export default {
         green_active: "#007A2E",
         white: "#fcfcfc",
         black: "#161616",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          1: "hsl(var(--chart-1))",
+          2: "hsl(var(--chart-2))",
+          3: "hsl(var(--chart-3))",
+          4: "hsl(var(--chart-4))",
+          5: "hsl(var(--chart-5))",
+        },
       },
       fontFamily: {
         secondaryRegular: ["Secondary-Regular"],
@@ -62,6 +110,12 @@ export default {
         primaryMedium: ["Primary-Medium"],
         primaryLight: ["Primary-Light"],
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
+  plugins: [require("tailwindcss-animate")],
 };
