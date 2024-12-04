1d13cd0e7f8668649a578fd4b58a5d6a937e9440
IamDalemark <danielweg123@gmail.com>
2024-12-05 02:10:07 +0800

    added middleware for change username

--- backend/src/routes/profile.ts
+++ backend/src/routes/profile.ts
@@ -1,8 +1,47 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { pool } from "..";
 
 const router = express.Router();
 // update name
+// get
+router.get(
+  "/",
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const { id } = request.query;
+      const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+        id,
+      ]);
+      response.status(200).json(result.rows);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+// update
+router.put(
+  "/",
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const { id } = request.query;
+      const { username } = request.body;
+
+      const result = await pool.query(
+        `
+        UPDATE users
+        SET username = $1
+        WHERE id = $2
+        RETURNING *
+         `,
+        [username, id],
+      );
+      response.status(200).json(result.rows);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 // update email
 
