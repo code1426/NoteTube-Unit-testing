ed44180ec84ed683bd6de3afb90593f226a14df3
Jose <joseearronn@gmail.com>
2024-12-07 04:09:46 +0800

    test ci

--- backend/src/utils/deleteFile.ts
+++ backend/src/utils/deleteFile.ts
@@ -2,7 +2,6 @@ import fs from "fs";
 
 const deleteFile = (file: Express.Multer.File) => {
   fs.unlink(file.path, (err) => {
-    // console.log("File deleted:", file.path);
     if (err) {
       throw new Error("Failed to delete file");
     }
