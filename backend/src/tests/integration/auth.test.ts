bdaa3e02aaaec5c4d725e11d64133e25cc46cb14
Jose <joseearronn@gmail.com>
2024-12-07 04:18:25 +0800

    Small fix

--- backend/src/tests/integration/auth.test.ts
+++ backend/src/tests/integration/auth.test.ts
@@ -4,18 +4,34 @@ import { app, pool } from "../../index";
 describe("The user registration endpoint", () => {
   const baseRoute = "/auth/register";
 
+  const testUser = {
+    username: "shish",
+    email: "shishwow@test.com",
+    password: "passwordKo12345",
+  };
+  const duplicateUsername = {
+    username: "shish",
+    email: "testing@test.com",
+    password: "passwordKo123456",
+  };
+  const duplicateEmail = {
+    username: "test2",
+    email: "shishwow@test.com",
+    password: "passwordKo1234567",
+  };
+
   afterAll(async () => {
-    await pool.query("DELETE FROM Users WHERE username='shish'"); // cleanup users table after testing all
+    await pool.query("DELETE FROM Users WHERE username IN ($1, $2)", [
+      // cleanup users table after testing all
+      testUser.username,
+      duplicateEmail.username,
+    ]);
     await pool.end(); // close the db connection after testing all
   });
 
   // ADDING A NEW USER
   it("should register a new user successfully", async () => {
-    const response = await request(app).post(baseRoute).send({
-      username: "shish",
-      email: "shishwow@test.com",
-      password: "passwordKo12345",
-    });
+    const response = await request(app).post(baseRoute).send(testUser);
 
     expect(response.status).toBe(201);
     expect(response.body).toBeDefined();
@@ -24,11 +40,7 @@ describe("The user registration endpoint", () => {
 
   // ADDING A NEW USER WITH THE SAME USERNAME
   it("should return an error if the username is already taken", async () => {
-    const response = await request(app).post(baseRoute).send({
-      username: "shish",
-      email: "testing@test.com",
-      password: "passwordKo123456",
-    });
+    const response = await request(app).post(baseRoute).send(duplicateUsername);
 
     expect(response.status).toBe(401);
     expect(response.body).toEqual({
@@ -39,11 +51,7 @@ describe("The user registration endpoint", () => {
 
   // ADDING A NEW USER WITH THE SAME EMAIL
   it("should return an error if the email is already in use", async () => {
-    const response = await request(app).post(baseRoute).send({
-      username: "test2",
-      email: "shishwow@test.com",
-      password: "passwordKo1234567",
-    });
+    const response = await request(app).post(baseRoute).send(duplicateEmail);
 
     expect(response.status).toBe(401);
     expect(response.body).toEqual({
