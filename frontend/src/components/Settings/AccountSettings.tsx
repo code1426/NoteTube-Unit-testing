1136e026681656f668f8ca23fc80c3be1f68b547
kailouss <kaizensomosera.04@gmail.com>
2024-12-08 23:03:56 +0800

    added switch account function, and icons

--- frontend/src/components/Settings/AccountSettings.tsx
+++ frontend/src/components/Settings/AccountSettings.tsx
@@ -1,6 +1,11 @@
 // import { useState } from "react";
 import UpdatePasswordForm from "./UpdatePasswordForm";
 import { Separator } from "@/components/ui/separator";
+import SwitchAccountConfirmation from "./SwitchAccountConfirmation";
+
+import { PiPasswordBold } from "react-icons/pi";
+import { RiDeleteBin5Fill } from "react-icons/ri";
+import { HiOutlineSwitchHorizontal } from "react-icons/hi";
 
 import {
   Dialog,
@@ -15,7 +20,7 @@ const AccountSettings = () => {
   return (
     <div
       id="main-container"
-      className="bg-white pb-2 h-full mt-4 text-center flex justify-start"
+      className="pb-2 h-full mt-4 text-center flex justify-start"
     >
       <div id="contents" className="space-y-2 w-full h-full">
         <Separator className=" bg-gray-300" />
@@ -23,8 +28,9 @@ const AccountSettings = () => {
           <DialogTrigger asChild>
             <div
               id="change-password"
-              className="w-full py-2 text-start text-md cursor-pointer hover:text-green ml-4 font-medium"
+              className="w-full py-2 text-start text-md cursor-pointer hover:text-green ml-4 font-medium flex flex-row"
             >
+              <PiPasswordBold size={20} className="self-center mr-2" />
               Change Password
             </div>
           </DialogTrigger>
@@ -33,18 +39,30 @@ const AccountSettings = () => {
             <UpdatePasswordForm />
           </DialogContent>
         </Dialog>
+
         <Separator className="my-4 bg-gray-300" />
-        <div
-          id="switch-account"
-          className="w-auto py-2 text-start text-md cursor-pointer hover:text-green ml-4  font-medium"
-        >
-          Switch Account
+
+        <div className="flex justify-start">
+          <SwitchAccountConfirmation>
+            <div
+              id="switch-account"
+              className="w-auto py-2 text-start text-md cursor-pointer hover:text-green ml-4 flex flex-row font-medium"
+            >
+              <HiOutlineSwitchHorizontal
+                size={20}
+                className="self-center mr-2"
+              />
+              Switch Account
+            </div>
+          </SwitchAccountConfirmation>
         </div>
+
         <Separator className="my-4 bg-gray-300" />
         <div
           id="delete-account"
-          className="text-md py-2 text-start cursor-pointer hover:text-red-600 ml-4  font-medium"
+          className="text-md py-2 text-start cursor-pointer hover:text-red-600 ml-4 flex flex-row font-medium"
         >
+          <RiDeleteBin5Fill size={20} className="self-center mr-2" />
           Delete Account
         </div>
         <Separator className="my-4 bg-gray-300" />
