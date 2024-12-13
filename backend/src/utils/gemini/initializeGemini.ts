7e31abf29345d2c60501b366b7b98d57c6488a62
Jose <joseearronn@gmail.com>
2024-12-13 22:10:06 +0800

    small fix

--- backend/src/utils/gemini/initializeGemini.ts
+++ backend/src/utils/gemini/initializeGemini.ts
@@ -1,17 +1,11 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { GoogleAIFileManager } from "@google/generative-ai/server";
 import * as dotenv from "dotenv";
-import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
 
 import generateSummary from "./outputSchemas/summarySchema";
 import generateFlashcards from "./outputSchemas/flashcardsSchema";
 
 dotenv.config();
-
-export const apiKey = process.env.GEMINI_API_KEY;
-if (!apiKey) {
-  throw new Error("GEMINI_API_KEY is not defined in the environment variables");
-}
 
 export const config = {
   summary: {
@@ -26,24 +20,10 @@ export const config = {
   },
 };
 
-const safetySettings = [
-  {
-    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-  },
-];
+export const apiKey = process.env.GEMINI_API_KEY;
+if (!apiKey) {
+  throw new Error("GEMINI_API_KEY is not defined in the environment variables");
+}
 
 const genAI = new GoogleGenerativeAI(apiKey);
 
@@ -52,7 +32,6 @@ export const model = genAI.getGenerativeModel({
   generationConfig: {
     responseMimeType: "application/json",
   },
-  safetySettings: safetySettings,
 });
 
 export const fileManager = new GoogleAIFileManager(apiKey);
