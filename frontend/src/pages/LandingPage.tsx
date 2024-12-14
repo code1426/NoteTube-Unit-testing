0fd15199906fa3d993e1729203197fd48f8c4b3f
kailouss <kaizensomosera.04@gmail.com>
2024-12-14 18:01:00 +0800

    updated landing page

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -17,18 +17,14 @@ const LandingPage = () => {
   const introductionRef = useRef<HTMLDivElement>(null);
   const discoverRef = useRef<HTMLDivElement>(null);
   const aboutRef = useRef<HTMLDivElement>(null);
-  const feature1Ref = useRef<HTMLDivElement>(null);
-  const feature2Ref = useRef<HTMLDivElement>(null);
-  const feature3Ref = useRef<HTMLDivElement>(null);
+  const featureRef = useRef<HTMLDivElement>(null);
   const faqsRef = useRef<HTMLDivElement>(null);
 
   const refs = {
     introductionRef,
     discoverRef,
     aboutRef,
-    feature1Ref,
-    feature2Ref,
-    feature3Ref,
+    featureRef,
     faqsRef,
   };
 
@@ -63,22 +59,47 @@ const LandingPage = () => {
     <div className="min-h-screen w-full overflow-auto flex flex-col items-center justify-center bg-cover bg-center bg-[url('/src/assets/images/paper-texture-bg.jpg')] bg-repeat scrollbar-custom">
       <LandingHeader refs={refs} />
       <Hero />
-      <div
-        ref={introductionRef}
-        className="section opacity-0 transform translate-y-24 transition-all duration-500 w-screen"
+      <section
+        className="section w-screen mx-8 shadow-2xl border-8"
+        ref={featureRef}
       >
-        <Section
-          title="What is NoteTube?"
-          description="NoteTube is your ultimate study companion, transforming notes into interactive learning resources."
-          reverse
-        />
-      </div>
+        <SectionCarousel duration={3000}>
+          <div className="section">
+            <Section
+              title="Upload Notes"
+              description="Break free from traditional note-taking limitations. Simply upload your handwritten, typed, or digital notes in any format - PDFs, images, text documents - and watch as NoteTube intelligently processes and transforms them into a comprehensive learning experience tailored just for you."
+              reverse
+              image="src/assets/images/notes.png"
+            />
+          </div>
+          <div className="section">
+            <Section
+              title="Get Related Videos"
+              description="Dive deeper into your learning with curated, intelligent video recommendations. Our AI analyzes your notes and finds the most relevant, high-quality educational content from across the web, ensuring you get targeted explanations that complement and enhance your existing study materials."
+              reverse
+              image="src/assets/images/videos.png"
+            />
+          </div>
+          <div className="section">
+            <Section
+              title="Use Generated Flashcards"
+              description="  Supercharge your memory retention with AI-powered flashcards. NoteTube automatically generates smart, context-aware flashcards from your notes, presenting key concepts, definitions, and critical information in an interactive format that makes studying more engaging and efficient."
+              reverse
+              image="src/assets/images/cards.png"
+            />
+          </div>
+        </SectionCarousel>
+      </section>
       {/*  */}
       <div
         ref={discoverRef}
         className="section opacity-0 transform translate-y-24 transition-all duration-500"
       >
         <InformationCardsSection />
+      </div>
+      {/*  */}
+      <div className="section opacity-0 transform translate-y-24 transition-all duration-500 w-screen">
+        <UserManualSection />
       </div>
       {/*  */}
       <div
@@ -88,51 +109,20 @@ const LandingPage = () => {
         <Section
           title="About Us"
           description="At NoteTube, we believe in revolutionizing the way students learn by integrating advanced AI technology. We transform scattered notes into structured, engaging study materials that are easy to comprehend and retain. Our mission is to enhance educational experiences by making studying more effective and enjoyable. With NoteTube, students can maximize their learning potential and achieve academic success."
-          reverse
+          reverse={false}
           image="src\assets\images\about.png"
         />
       </div>
       {/*  */}
-      <section className="section w-screen mx-8 shadow-2xl border-8">
-        <SectionCarousel duration={3000}>
-          <div className="section">
-            <Section
-              title="Upload Notes"
-              description="Break free from traditional note-taking limitations. Simply upload your handwritten, typed, or digital notes in any format - PDFs, images, text documents - and watch as NoteTube intelligently processes and transforms them into a comprehensive learning experience tailored just for you."
-              reverse={false}
-              image="src/assets/images/notes.png"
-            />
-          </div>
-          <div className="section">
-            <Section
-              title="Get Related Videos"
-              description="Dive deeper into your learning with curated, intelligent video recommendations. Our AI analyzes your notes and finds the most relevant, high-quality educational content from across the web, ensuring you get targeted explanations that complement and enhance your existing study materials."
-              reverse={false}
-              image="src/assets/images/videos.png"
-            />
-          </div>
-          <div className="section">
-            <Section
-              title="Use Generated Flashcards"
-              description="  Supercharge your memory retention with AI-powered flashcards. NoteTube automatically generates smart, context-aware flashcards from your notes, presenting key concepts, definitions, and critical information in an interactive format that makes studying more engaging and efficient."
-              reverse={false}
-              image="src/assets/images/cards.png"
-            />
-          </div>
-        </SectionCarousel>
-      </section>
-      {/*  */}
-      <div className="section opacity-0 transform translate-y-24 transition-all duration-500 w-screen">
-        <UserManualSection />
+      <div className="section opacity-0 transform translate-y-24 transition-all duration-500">
+        <JoinUsSection />
       </div>
+      {/*  */}
       <div
         ref={faqsRef}
         className="section opacity-0 transform translate-y-24 transition-all duration-500 w-auto"
       >
         <FAQs />
-      </div>
-      <div className="section opacity-0 transform translate-y-24 transition-all duration-500">
-        <JoinUsSection />
       </div>
       <Footer />
     </div>
