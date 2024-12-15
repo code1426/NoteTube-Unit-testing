dbe28d8b79efc2cfec05d19cb5181b1bd4f533c0
Jose <joseearronn@gmail.com>
2024-12-15 22:25:54 +0800

    Initialize frontend testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
@@ -5,6 +8,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    include: ["**/*.test.{ts,tsx}"],
+    globals: true,
+    environment: "jsdom",
+    css: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
