0da648b0e07cc53d2bef9afe65a3048d19dc9a3f
Jose <joseearronn@gmail.com>
2024-12-19 04:02:40 +0800

    add deck hooks tests

---
+++ frontend/src/mocks/data.ts
@@ -0,0 +1,51 @@
+export const mockDecks = [
+  {
+    id: "1",
+    deck_name: "test1",
+    user_id: "1",
+    card_count: 1,
+    color: "green",
+    created_at: "2024-01-01T00:00:00Z",
+  },
+  {
+    id: "2",
+    deck_name: "test2",
+    user_id: "1",
+    card_count: 5,
+    color: "red",
+    created_at: "2024-01-02T00:00:00Z",
+  },
+  {
+    id: "3",
+    deck_name: "test3",
+    user_id: "2",
+    card_count: 5,
+    color: "yellow",
+    created_at: "2024-01-02T00:00:00Z",
+  },
+  {
+    id: "4",
+    deck_name: "test4",
+    user_id: "2",
+    card_count: 5,
+    color: "blue",
+    created_at: "2024-01-02T00:00:00Z",
+  },
+  {
+    id: "5",
+    deck_name: "test5",
+    user_id: "3",
+    card_count: 5,
+    color: "green",
+    created_at: "2024-01-02T00:00:00Z",
+  },
+];
+
+export const mockAddDeck = {
+  id: "5",
+  deck_name: "add1",
+  user_id: "1",
+  card_count: 4,
+  color: "white",
+  created_at: "2024-01-01T00:00:00Z",
+};
