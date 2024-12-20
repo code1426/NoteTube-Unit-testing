ff8c0a3cae8b8305ad841402206dbdf968676b95
code1426 <gmver27@gmail.com>
2024-12-21 00:15:59 +0800

    small fix

--- frontend/src/context/ThemeProvider.tsx
+++ frontend/src/context/ThemeProvider.tsx
@@ -1,21 +1,46 @@
 import { Theme } from "@/types/theme.types";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ThemeContext } from "./Contexts";
 
-interface ThemeProviderProps {
+type ThemeProviderProps = {
   children: React.ReactNode;
-}
+  defaultTheme?: Theme;
+  storageKey?: string;
+};
 
-const ThemeProvider = ({ children }: ThemeProviderProps) => {
-  const [theme, setTheme] = useState<Theme>("light");
+const ThemeProvider = ({
+  children,
+  defaultTheme = "light",
+  storageKey = "theme",
+}: ThemeProviderProps) => {
+  const [theme, setTheme] = useState<Theme>(() => {
+    const storedTheme = localStorage.getItem(storageKey);
+    return (storedTheme as Theme) || defaultTheme;
+  });
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.remove("light", "dark");
+
+    if (theme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light";
+
+      root.classList.add(systemTheme);
+      return;
+    }
+
+    root.classList.add(theme);
+  }, [theme]);
 
   const value = {
     theme,
-    setTheme: (theme: Theme) => {
-      const root = window.document.documentElement;
-      root.classList.remove("light", "dark");
-      root.classList.add(theme);
-      setTheme(theme);
+    setTheme: (newTheme: Theme) => {
+      localStorage.setItem(storageKey, newTheme);
+      setTheme(newTheme);
     },
   };
 
